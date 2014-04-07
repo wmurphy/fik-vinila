d8b22bf30345a1360be88c62048e78d7963e7874
elenago <elenagomezcaballero@gmail.com>
2014-04-07 15:06:22 +0200

    maquinación, hovers, actives, menús, heder, footer, plantilla

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,15 +1,8 @@
 <div class="gallery">
 	<img src="/wp-content/themes/fik-vinila/assets/img/imgslider.jpg">
 </div>
-<article class="homecontent container">
-	<section class="col-lg-6 element izq no-padding-left">
-		<a href="#">Vinila World</a>
-	</section>	
-	<section class="col-lg-6 element dcha no-padding-right">
-		<a href="#">Oferta de la semana</a>
-	</section>
-</article>
+
 <?php while (have_posts()) : the_post(); ?>
-  <!--<?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>-->
+  <?php the_content(); ?>
+  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
