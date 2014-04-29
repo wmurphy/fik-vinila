0945fcea1e981e8d0dbd7cd824371430fc484fbe
minayaserrano <minayaserrano@gmail.com>
2014-04-29 19:16:43 +0200

    Add js: carousel and product page

---
+++ assets/js/fik_product.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $('.sizes').hide();
+  $('.shippings').hide();
+  $('.sizesandshippingsmodaloverlay').hide();
+
+  $('.sizesinformation').click(function(event) {
+      event.preventDefault();
+      $('.shippings').hide();
+      $('.sizes').toggle();
+      $('.sizesandshippingsmodaloverlay').toggle();
+  });
+  $('.shippingsinformation').click(function(event) {
+      event.preventDefault();
+      $('.sizes').hide();
+      $('.shippings').toggle();
+      $('.sizesandshippingsmodaloverlay').toggle();
+  });
+  $('.close').click(function(event) {
+      $('.sizes').hide();
+      $('.shippings').hide();
+      $('.sizesandshippingsmodaloverlay').hide();
+  });
+  $('.sizesandshippingsmodaloverlay').click(function(event) {
+      $('.close').click();
+  });
+
+  $('.carousel').carousel();
+});
