3705fa4c434edf4d9675c003f3abfdacd2e04982
Elena <elenagomezcaballero@gmail.com>
2014-04-29 21:53:20 +0200

    responsive menu superior y footer completos

--- templates/footer.php
+++ templates/footer.php
@@ -17,7 +17,7 @@
 	        	<?php wp_nav_menu(array('theme_location' => 'footer-vinila', 'menu_class' => 'nav f-menu')); ?>
 	    	</div>
 	    	<div class="logofooter pull-left"><img src="/wp-content/themes/fik-vinila/assets/img/logosmall.png"></div>
-		    <p class="col-sm-offset-9 copyr">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> </p>
+		    <p class="copyr">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> <span class="rights">Todos los derechos reservados</span></p>
 		</div>
 		
 	</div>
