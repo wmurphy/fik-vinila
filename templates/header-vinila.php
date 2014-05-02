10df4caafadf830feed7dbb2b93c5c8a5372f43b
Elena <elenagomezcaballero@gmail.com>
2014-05-02 16:51:35 +0200

    imágenes de bolsa y lobo del menú

--- templates/header-vinila.php
+++ templates/header-vinila.php
@@ -6,24 +6,30 @@
           <?php the_store_logo(null, array('class' => 'logo')); ?>
         </a>
       </div> 
-      <div class="col-sm-3 pull-right">
+      <div class="col-sm-3 pull-right language">
           <div class="idioma">
             <?php wp_nav_menu(array('theme_location' => 'language', 'menu_class' => 'nav lang')); ?>
           </div>
-          <div class="bolsa">
-            <?php wp_nav_menu(array('theme_location' => 'bag', 'menu_class' => 'nav menu-bag')); ?> 
-          </div>
+
         </div>
     </div>
       </div>
     <div class="col-lg-12 menuprin">
-      <nav class="nav-main container" role="navigation">
+      <div class="container">
+      <div class="logomenu col-sm-2">
+        <a href="/" title="Vinila Fabrics"><img src="/wp-content/themes/fik-vinila/assets/img/logomenu.png" alt="Logo Vinila" title="Logo Vinila"></a>
+      </div>
+      <nav class="nav-main col-sm-8" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills'));
           endif;
-        ?>
+          ?>
       </nav>
+      <div class="bolsa col-sm-2">
+        <?php wp_nav_menu(array('theme_location' => 'bag', 'menu_class' => 'nav menu-bag')); ?> 
+      </div>
+    </div>
     </div>
 
 </header>
