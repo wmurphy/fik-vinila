7ec92ebcccc7fd6ef4713983b6ef88a4a3d8bfcd
Bill Murphy <murphy@litio.net>
2014-05-16 12:00:39 +0200

    Better zoom js

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,23 +1,18 @@
 /**
 * zoom.js is a JS enabling magnifying glass effect on product images and gallery effect.
 */
-$(window).load(activatezoom);
-$(window).resize(function(){$(".zoomContainer").remove(); activatezoom();});
+$(window).load(vinilazoom);
+$(window).resize(function(){$(".zoomContainer").remove(); vinilazoom();});
 
-function activatezoom(zoomimagewidth) {
-    jQuery('.zoomContainer').remove();
-    if (jQuery('div.product-image-frame img').width() < (zoomimagewidth)*0.77) {
-        jQuery('div.product-image-frame img').elevateZoom({
-            zoomType : "lens",
-            lensShape : "square",
-            lensSize : 250
-        });
-    } else {
-        jQuery('.zoomLens').css('opacity','0');
-    }
+function vinilazoom() {
+    $(".product-image-thumbnails a").click(function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        $("#prod-img").attr("src", $(this).attr("data-zoom-image"));
+        $("#prod-img").data("zoom-image", $(this).data("zoom-image"));
+    });
+
+    if($(window).width() < 993) return false;
+    // Add zoom to the new image
+    $("#prod-img").elevateZoom({responsive: true, scrollZoom:true, gallery:'thumbnails-gallery', zoomWindowWidth:$("#prod-img").width(), zoomWindowHeight:$("#prod-img").height(), zoomWindowOffetx: 15, tint:true, tintColour:'#ccc', tintOpacity:0.5, zoomWindowFadeIn: 500, zoomWindowFadeOut: 500, lensFadeIn: 200, lensFadeOut: 300, easing : true});
 }
-
-jQuery(document).ready(function() {
-    activatezoom(jQuery('div.product-image-frame img').data('zoomimagewidth'));
-});
-
