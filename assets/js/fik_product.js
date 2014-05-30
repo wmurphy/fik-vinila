c136b1abf8de79a94d6e8f9083b016de1ee1a08f
Bill Murphy <murphy@litio.net>
2014-05-30 17:12:47 +0200

    Call jQuery

--- assets/js/fik_product.js
+++ assets/js/fik_product.js
@@ -1,13 +1,13 @@
 function activatezoom(zoomimagewidth) {
-    $('.zoomContainer').remove();
-    if ($('div.product-image-frame img').width() < (zoomimagewidth)*0.77) {
-        $('div.product-image-frame img').elevateZoom({
+    jQuery('.zoomContainer').remove();
+    if (jQuery('div.product-image-frame img').width() < (zoomimagewidth)*0.77) {
+        jQuery('div.product-image-frame img').elevateZoom({
             zoomType : "lens",
             lensShape : "square",
             lensSize : 250
         });
     } else {
-        $('.zoomLens').css('opacity','0');
+        jQuery('.zoomLens').css('opacity','0');
     }
 }
 
@@ -15,7 +15,6 @@ $(document).ready(function() {
   $('.sizes').hide();
   $('.shippings').hide();
   $('.sizesandshippingsmodaloverlay').hide();
-  activatezoom($('div.product-image-frame img').data('zoomimagewidth'));
 
   $('.sizesinformation').click(function(event) {
       event.preventDefault();
@@ -47,7 +46,7 @@ $(document).ready(function() {
   $(".product-image-thumbnails a").click(function(event) {
     $("#prod-img").attr("src", $(this).attr("data-zoom-image"));
     $("#prod-img").data("zoom-image", $(this).data("zoom-image"));
-    activatezoom($('div.product-image-frame img').data('zoomimagewidth'));
+    activatezoom(jQuery('div.product-image-frame img').data('zoomimagewidth'));
     // prevent href
     return false;
   }); 
