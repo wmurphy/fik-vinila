f09a1a9b4ff9244a88a1e68a8d8ba126ec257136
minayaserrano <minayaserrano@gmail.com>
2014-06-03 17:41:19 +0200

    Footer design changes

--- templates/footer.php
+++ templates/footer.php
@@ -1,25 +1,27 @@
 <footer class="content-info" role="contentinfo">
 	<div class="container">
-		<aside class="f-social col-md-4 pull-right">
-			<?php dynamic_sidebar('sidebar-news'); ?>
-			<div class="social">     
-				<p>Síguenos en:</p> 
-			  	<?php
-		        	if (has_nav_menu('social-menu')) :
-		          		wp_nav_menu(array('theme_location' => 'social-menu', 'menu_class' => 'nav social pull-left'));
-		        	endif;
-		      	?>
-		      	<div class="pull-right partelogo"><a href="http://fikstores.com/" title="tiendas online"><img class="replace-2x fikstores-badge" width="105" height="50" alt="Better ecommerce" src="/wp-content/themes/fik-vinila/assets/img/logofik.png"></a></div>
-	    	</div>
-		</aside>
-		<div class="col-md-8">
+		<div class="col-md-5">
 			<div class="menu-footer">
 	        	<?php wp_nav_menu(array('theme_location' => 'footer-vinila', 'menu_class' => 'nav f-menu')); ?>
 	    	</div>
-		    <p class="copyr">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> <span class="rights">Todos los derechos reservados</span></p>
 		</div>
 		
+		<aside class="f-social col-md-7 pull-right">
+            <div class="col-md-6">
+			<?php dynamic_sidebar('sidebar-news'); ?>
+            </div>
+			<div class="social col-md-6 text-right">     
+			  	<?php
+		        	if (has_nav_menu('social-menu')) :
+		          		wp_nav_menu(array('theme_location' => 'social-menu', 'menu_class' => 'nav social pull-right'));
+		        	endif;
+		      	?>
+	    	</div>
+		</aside>
 	</div>
 </footer>
+        <div class="col-md-12 text-center">
+            <a href="http://fikstores.com/" title="tiendas online"><img class="replace-2x fikstores-badge" width="105" height="50" alt="Better ecommerce" src="/wp-content/themes/fik-vinila/assets/img/logofik.png"></a>
+        </div>
 
 <?php wp_footer(); ?>
