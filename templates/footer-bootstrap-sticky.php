2738e05be895ffd218408c4bb022c99363ec5302
Bill Murphy <murphy@litio.net>
2014-06-13 18:37:20 +0200

    Sticky footer with everything

--- templates/footer-bootstrap-sticky.php
+++ templates/footer-bootstrap-sticky.php
@@ -1,13 +1,42 @@
+
+
 <div style="padding-top: 115px"></div>
 <footer class="content-info navbar navbar-default navbar-fixed-bottom" role="contentinfo">
-  <div class="container">
-    <div class="col-xs-12 col-mg-5">
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+
+    <hr>
+	<div class="container">
+		<div class="col-md-6">
+			<div class="menu-footer">
+				<?php
+				if (has_nav_menu('footer-vinila')) {
+	        	 wp_nav_menu(array('theme_location' => 'footer-vinila', 'menu_class' => 'nav f-menu')); 
+	        	}
+	        	?>
+	    	</div>
+		</div>
+		
+		<aside class="f-social col-md-6 pull-right">
+            <div class="col-md-6">
+			<?php dynamic_sidebar('sidebar-news'); ?>
+            </div>
+			<div class="social col-md-6 text-right">     
+			  	<?php
+		        	if (has_nav_menu('social-menu')) :
+		          		wp_nav_menu(array('theme_location' => 'social-menu', 'menu_class' => 'nav social pull-right'));
+		        	endif;
+		      	?>
+	    	</div>
+		</aside>
+	</div>
+
+<div class="container">
+    <div class="col-md-12 text-left">
+        <a href="http://fikstores.com/" title="tiendas online"><img class="replace-2x fikstores-badge" width="105" height="50" alt="Better ecommerce" src="/wp-content/themes/fik-vinila/assets/img/logofik.png"></a>
     </div>
-    <div class="col-xs-12 col-mg-7 right text-right">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-    </div>
-    <?php wp_footer(); ?>
-  </div>
+</div>
+
 </footer>
+
+
+<?php wp_footer(); ?>
 
